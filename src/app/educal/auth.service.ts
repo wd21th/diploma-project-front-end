ef4f94361041b1860f0be582a308208a0b1260ac
Dias <zhigittt@mail.ru>
2023-03-28 22:15:02 +0600

    tap debuger

--- src/app/educal/auth.service.ts
+++ src/app/educal/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,15 @@ constructor(
 ) { }
 
   check(user: any){
-    return this.http.post(environment.backendUrl + '/api/login', user);
+    return this.http.post(environment.backendUrl + '/api/login', user).pipe(
+      tap((res) => console.log(res))
+    )
   }
 
   add(user: any){
-    return this.http.post(environment.backendUrl + '/api/register', user);
+    return this.http.post(environment.backendUrl + '/api/register', user).pipe(
+      tap((res) => console.log(res))
+    )
   }
 
 }
